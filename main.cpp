84daf0ef4c0ba55a0b09e7e3a6cb3881d4db287c
Manjot Singh <95271481+Manjot-2000@users.noreply.github.com>
2023-03-21 14:15:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ using std :: cout , std :: endl, std :: vector;
 int main() 
 {
     set_parameters();
-    std :: unique_ptr<Account> ptr {std :: make_unique<Checking_account> ("Ramos", 2000)};
-    std :: unique_ptr<Account> ptr1 {std :: make_unique<Trust_Account> ("Forvitos", 10000, 15)};
-    std :: unique_ptr<Account> ptr2 {std :: make_unique<Savings_account> ("Fraxture", 5000, 10)};
+    std :: unique_ptr<Account> ptr {std :: make_unique<Checking_account> ("Harjit Singh", 2000)};
+    std :: unique_ptr<Account> ptr1 {std :: make_unique<Trust_Account> ("Samir Sharma", 10000, 15)};
+    std :: unique_ptr<Account> ptr2 {std :: make_unique<Savings_account> ("Sahil Sharma", 5000, 10)};
 
     vector<std :: unique_ptr<Account>> accounts;
-    accounts.push_back(std :: make_unique<Checking_account> ("Ramos", 2000));
-    accounts.push_back(std :: make_unique<Trust_Account> ("Forvitos", 10000, 15));
+    accounts.push_back(std :: make_unique<Checking_account> ("Biswas Singh", 2000));
+    accounts.push_back(std :: make_unique<Trust_Account> ("Rohit Mehra", 10000, 15));
     accounts.push_back(std :: move(ptr));
 
     for (const auto &i : accounts) {
